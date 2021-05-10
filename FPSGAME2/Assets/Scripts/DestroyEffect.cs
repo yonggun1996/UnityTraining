7bb1deaf3f23cf1dd0fdd4654943a7fc4d25be67
qkrdydrjs7@naver.com <qkrdydrjs7@naver.com>
2021-05-10 18:06:59 +0900

    5월 10일 실습 코드 입니다.
    hp바 구현, 적을 쐈을 때 hp 감소와 사라지는것 구현, 적이 나를 발견했을 때 따라오게,  20m 떨어지면 적이 따라오지 않게 하는 방법을 실습했습니다.

---
+++ FPSGAME2/Assets/Scripts/DestroyEffect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyEffect : MonoBehaviour
+{
+    public float destroyTime = 1.5f;
+    float currentTime = 0;
+
+  
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentTime > destroyTime)
+        {
+            Destroy(gameObject);
+        }
+        currentTime += Time.deltaTime;
+    }
+}
---
+++ FPSGAME2/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float moveSpeed = 7f;
+    CharacterController cc;
+    float gravity = -20f; //중력 변수
+    float yVelocity = 0f; //수직 속력 변수
+    public float jumpPower = 10f;
+    public bool isJumping = false;
+    public int hp = 100;//플레이어의 체력
+    int maxHp = 20;
+    public Slider hpSlider;
+    public GameObject hitEffect;
+    
+    private void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 1. 사용자 입력
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        //2. 이동 방향 설정
+        Vector3 dir = new Vector3(h, 0, v);
+        dir = dir.normalized; // 정규화
+       // 3. 이동속도에 맞춰 이동한다. p =p0 + vt
+        dir = Camera.main.transform.TransformDirection(dir);
+        //transform.position += dir * moveSpeed * Time.deltaTime;
+
+        if (cc.collisionFlags == CollisionFlags.Below)
+        {
+            if (isJumping)
+            {
+                isJumping = false;
+                //yVelocity = 0;
+            }
+            yVelocity = 0;
+        }
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            yVelocity = jumpPower;
+            isJumping = true;
+        }
+
+        // 캐릭터 수직 속도에 중력 값을 적용한다.
+        yVelocity += gravity * Time.deltaTime;
+        dir.y = yVelocity;
+      //  print("yVelocity: " + yVelocity);
+        cc.Move(dir * moveSpeed * Time.deltaTime);
+        hpSlider.value = (float)hp / (float)maxHp;
+    }
+
+    public void DamageAction(int damage)
+    {
+        hp -= damage;
+        if(hp > 0)
+        {
+            StartCoroutine(PlayHitEffect());
+        }
+    }
+
+    IEnumerator PlayHitEffect()
+    {
+        hitEffect.SetActive(true);
+        yield return new WaitForSeconds(0.3f);
+        hitEffect.SetActive(false);
+    }
+}
